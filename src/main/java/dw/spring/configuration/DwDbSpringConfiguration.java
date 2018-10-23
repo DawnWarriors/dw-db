b33649b29585c97f57abb5ce5f0dd25c7fa87cf9
曹燕飞 <cyf2018@sina.com>
2018-10-23 11:34:03 +0800

    更新POM中spring依赖版本

--- src/main/java/dw/spring/configuration/DwDbSpringConfiguration.java
+++ src/main/java/dw/spring/configuration/DwDbSpringConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DwSpringConfiguration
+public class DwDbSpringConfiguration
 {
 	@Bean
 	public DwDbSpringListener getDwDbSpringListener()
